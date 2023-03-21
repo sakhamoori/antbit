a08df8e9ba1263bdb69611ffa5375da4af9cedb5
Amigo <rakesh.sr@gmail.com>
2023-03-21 11:27:02 -0700

    Updates

--- src/components/Miners.tsx
+++ src/components/Miners.tsx
@@ -104,11 +104,11 @@ const Miners = () => {
                     stroke="#000000#b2aeae"
                     transform="rotate(180)"
                   >
-                    <g id="SVGRepo_bgCarrier" stroke-width="0" />
+                    <g id="SVGRepo_bgCarrier" strokeWidth="0" />
                     <g
                       id="SVGRepo_tracerCarrier"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                     />
                     <g id="SVGRepo_iconCarrier">
                       {' '}
