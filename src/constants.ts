1cd8f15d4209b9490bf4d85b59b2c01751cdcafe
Amigo <rakesh.sr@gmail.com>
2023-03-24 18:19:11 -0700

    Missing Solana file

---
+++ src/constants.ts
@@ -0,0 +1,18 @@
+export const RPC_URL: string = `https://rinkeby.infura.io/v3/${process.env.REACT_APP_INFURA_ID}`;
+
+export const RPC_URL_MAINNET: string = `https://mainnet.infura.io/v3/${process.env.REACT_APP_INFURA_ID}`;
+export const RPC_URL_KOVAN: string = `https://kovan.infura.io/v3/${process.env.REACT_APP_INFURA_ID}`;
+export const RPC_URL_ROPSTEN: string = `https://ropsten.infura.io/v3/${process.env.REACT_APP_INFURA_ID}`;
+export const RPC_URL_RINKEBY: string = `https://rinkeby.infura.io/v3/${process.env.REACT_APP_INFURA_ID}`;
+export const RPC_URL_GOERLI: string = `https://goerli.infura.io/v3/${process.env.REACT_APP_INFURA_ID}`;
+
+export const CHAIN_ID_MAINNET: number = 1; // Mainnet
+export const CHAIN_ID_KOVAN: number = 42; // Kovan
+export const CHAIN_ID_ROPSTEN: number = 3; // Ropsten
+export const CHAIN_ID_RINKEBY: number = 4; // Rinkeby
+export const CHAIN_ID_GOERLI: number = 5; // Goerli
+
+export const ACCOUNT2: string = 'xxxxx';
+
+export const ACCOUNT2_SOL: string = 'xxxxx';
+export const LAMPORTS_PER_SOL: number = 100;
