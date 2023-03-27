ee0e4e972ce771fdededc984364a2773db5b5243
Amigo <rakesh.sr@gmail.com>
2023-03-27 14:07:59 -0700

    Cleaning up the UI

--- src/components/Miners.tsx
+++ src/components/Miners.tsx
@@ -47,16 +47,17 @@ const Miners = () => {
   };
 
   const renderState = (status: string) => {
-    if (status !== 'offline' && status !== 'disabled') {
+    if (status === 'offline' || status === 'disabled') {
       return (
         <img
-          src="https://gauelondvgmzvqfdmhco.supabase.co/storage/v1/object/public/cloud/online.png?t=2023-03-18T00%3A43%3A17.461Z"
+          src="https://gauelondvgmzvqfdmhco.supabase.co/storage/v1/object/public/cloud/destroyed.png?t=2023-03-18T00%3A42%3A56.337Z"
           width="25px"
           height="25px"
           title={status}
         />
       );
     }
+
     return (
       <img
         src="https://gauelondvgmzvqfdmhco.supabase.co/storage/v1/object/public/cloud/online.png?t=2023-03-18T00%3A43%3A17.461Z"
@@ -85,81 +86,11 @@ const Miners = () => {
             </div>
             <div className="space-y-2 divide-y divide-dotted divide-zinc-600 leading-10">
               <div className="grid grid-cols-2">
-                <div className="flex">
-                  Idle Hours
-                  <svg
-                    width="20px"
-                    height="20px"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    stroke="#000000#b2aeae"
-                    transform="rotate(180)"
-                  >
-                    <g id="SVGRepo_bgCarrier" strokeWidth="0" />
-                    <g
-                      id="SVGRepo_tracerCarrier"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    />
-                    <g id="SVGRepo_iconCarrier">
-                      {' '}
-                      <path
-                        d="M12 7C12.5523 7 13 7.44772 13 8V13C13 13.5523 12.5523 14 12 14C11.4477 14 11 13.5523 11 13V8C11 7.44772 11.4477 7 12 7Z"
-                        fill="#b2aeae"
-                      />{' '}
-                      <path
-                        d="M12 17C12.5523 17 13 16.5523 13 16C13 15.4477 12.5523 15 12 15C11.4477 15 11 15.4477 11 16C11 16.5523 11.4477 17 12 17Z"
-                        fill="#b2aeae"
-                      />{' '}
-                      <path
-                        fillRule="evenodd"
-                        clipRule="evenodd"
-                        d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2ZM4 12C4 7.58172 7.58172 4 12 4C16.4183 4 20 7.58172 20 12C20 16.4183 16.4183 20 12 20C7.58172 20 4 16.4183 4 12Z"
-                        fill="#b2aeae"
-                      />{' '}
-                    </g>
-                  </svg>
-                </div>
+                <div>Idle Hours</div>
                 <div className="text-right">{worker.hours_idle} hrs</div>
               </div>
               <div className="grid grid-cols-2">
-                <div className="flex">
-                  Compute Hours
-                  <svg
-                    width="20px"
-                    height="20px"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    stroke="#000000#b2aeae"
-                    transform="rotate(180)"
-                  >
-                    <g id="SVGRepo_bgCarrier" strokeWidth="0" />
-                    <g
-                      id="SVGRepo_tracerCarrier"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    />
-                    <g id="SVGRepo_iconCarrier">
-                      {' '}
-                      <path
-                        d="M12 7C12.5523 7 13 7.44772 13 8V13C13 13.5523 12.5523 14 12 14C11.4477 14 11 13.5523 11 13V8C11 7.44772 11.4477 7 12 7Z"
-                        fill="#b2aeae"
-                      />{' '}
-                      <path
-                        d="M12 17C12.5523 17 13 16.5523 13 16C13 15.4477 12.5523 15 12 15C11.4477 15 11 15.4477 11 16C11 16.5523 11.4477 17 12 17Z"
-                        fill="#b2aeae"
-                      />{' '}
-                      <path
-                        fillRule="evenodd"
-                        clipRule="evenodd"
-                        d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2ZM4 12C4 7.58172 7.58172 4 12 4C16.4183 4 20 7.58172 20 12C20 16.4183 16.4183 20 12 20C7.58172 20 4 16.4183 4 12Z"
-                        fill="#b2aeae"
-                      />{' '}
-                    </g>
-                  </svg>
-                </div>
+                <div>Compute Hours</div>
                 <div className="text-right">{worker.hours_hired} hrs</div>
               </div>
               <div className="grid grid-cols-2">
